ecdc168633e3662a1e8651f5799f839e9996c476
Natalie S <nataliesanchez42204@gmail.com>
2025-06-17 08:47:38 -0400

    Updates to this

--- main.py
+++ main.py
@@ -1,50 +1,43 @@
-#Practicing my Python knowledge by creating a task planner app.
-#TODO.
-#INPUT A TEXT WELCOMING USER.. (DONE)
-#Input task name entry(DONE)
-#input add
-#input delete
-#input mark as done
-#input retrieve
-#input view of tasks
+#Practicing my Python knowledge.
 
-
-#What imports???
-#For now it is in terminal. DO I WANT TO MAKE IT INTO A GUI?? (WAIT!!)
+user = {}
+#For now it is in terminal. DO I WANT TO MAKE IT INTO A GUI??
 def login():
 #LOGIN
     userName = input("""
-    Please input your username: """)
+    Please input your username: """) #This will grab the username from the user dict
 
-    userPass = input("Please Insert your password: ")
+    userPass = input("Please Insert your password: ") 
 
-    if userPass == "Test":
-        #THIS WILL NOT STAY HERE IT WILL GRAB THE USER'S PASSWORD FROM A FILE OR SOMEWHERE!!
+    if userPass == "Test": #This will grab the users password from the user dict
 
         print("Welcome ", userName)
-        #will set to continue to app () later on
     else: 
         print("Inccorect password.")
 
 
 #Create account
 def createAccount():
-    #TODO ADD MY LOGIC FOR CREATING ACCOUNT
-    #MAKE IT SAVE THE ACCOUNT INFORMATION.
-    #I HAVE TO RESEARCH HOW.
-    #TODO Create a DB to store Username and password??? do I really need to?
-    pass
+    user_email = input("Please inser an email: ")
+    user_name = input("Please insert a username: ")
+    user_password = input("Please insert a password: ")
+    
+
+    #Save it to the user dictionary
+    #make sure there isnt an account already made
+
+    print("You have created an account!")
 
 
 print("""--------------------------
 Welcome to the tasks planners!
 ------------------------------------""")
-input("""Press 1 to sign in.
+userselect = input("""Press 1 to sign in.
 Press 2 to Create Account.
 """)
 
-if "1":
+if  userselect == "1":
     login()
 else:
-    if "2":
+    if userselect == "2":
         createAccount()
